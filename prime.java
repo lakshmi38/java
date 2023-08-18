b56f2fc63c7f8cb5e62b3eeca78ad58ad391d028
lakshmi38 <121122744+lakshmi38@users.noreply.github.com>
2023-08-18 16:05:00 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,28 @@
+package com.edu;
+
+import java.util.Scanner;
+public class prime {
+	public static void main(String[] args) {
+		
+		int num, count=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		num=sc.nextInt(); //num=6 , 1 2 3 6
+		
+		for(int i=1;i<=num;i++) { //6%1, 6%2, 6%3, 6%4 6%5 6%6
+			if(num%i == 0) {
+				count++;
+				//count = count+1
+			
+			}
+		}
+		
+		if(count == 2) {
+			System.out.println(num+" is prime number");
+		}
+		else {
+			System.out.println(num+" is not prime number");
+		}
+	}
+
+}
